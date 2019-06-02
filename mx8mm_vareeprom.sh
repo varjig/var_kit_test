6d8055660a51d2decb2df840074532e679059ceb
Felix Radensky <felix.r@variscite.com>
2019-06-02 10:55:40 +0300

    mx8mm_vareeprom.sh: Use i2c tools to program EEPROM

--- mx8mm_vareeprom.sh
+++ mx8mm_vareeprom.sh
@@ -1,62 +1,71 @@
 #!/bin/bash
 
-EEPROM_SYSFS=/sys/devices/platform/30a20000.i2c/i2c-0/0-0052/eeprom
+I2C_BUS=0
+I2C_ADDR=0x52
 
 # Part number offset and size
-PN_OFFSET=2
+PN_OFFSET=0x02
 PN_LEN=3
 
 # Assembly offset and size
-AS_OFFSET=5
+AS_OFFSET=0x05
 AS_LEN=10
 
 # Date offset and size
-DATE_OFFSET=15
+DATE_OFFSET=0x0f
 DATE_LEN=9
 
 # MAC offset and size
-MAC_OFFSET=24
+MAC_OFFSET=0x18
 MAC_LEN=6
 
 # SOM revision offset and size
-SR_OFFSET=30
+SR_OFFSET=0x1e
 SR_LEN=1
 
+# EEPROM version offsetand size
+VER_OFFSET=0x1f
+VER_LEN=1
+
 # SOM options offset and size
-OPT_OFFSET=32
+OPT_OFFSET=0x20
 OPT_LEN=1
 
 # EEPROM Field Values
-SOM_OPTIONS="0f"
+SOM_OPTIONS="0x0f"
+SOM_REV="0x01"
 
 # params:
-# 1: data addr
-# 2: value
+# 1: i2c bus
+# 2: chip addr
+# 3: data addr
+# 4: value
 write_i2c_byte()
 {
-	echo -n -e \\x$2 | dd of=${EEPROM_SYSFS} bs=1 seek=$1 2>/dev/null
+	i2cset -r -y $1 $2 $3 $4 > /dev/null
 	usleep 5000
-	VAL=$(dd if=${EEPROM_SYSFS} bs=1 skip=$1 count=1 2>/dev/null | od -t x1 | head -n 1 | awk '{print $2}')
-	if [ "$VAL" != "$2" ]; then
-		echo "FAIL! EEPROM VERIFY ADDRESS $1 SHOULD BE $2. READ $VAL."
+	VAL=`i2cget -y $1 $2 $3`
+	if [ "$VAL" != "$4" ]; then
+		echo "FAIL! EEPROM VERIFY ADDRESS $3 SHOULD BE $4. READ $VAL."
 		exit 1
 	fi
 }
-
 
 # This func doesn't write a trailing
 # zero at the end of the string
 #
 # params:
-# 1: data addr
-# 2: value
+# 1: i2c bus
+# 2: chip addr
+# 3: data addr
+# 4: value
 write_i2c_string()
 {
-	DATA_ADDR=$1
-	A=$(echo $2 | awk NF=NF FS=)
+	DATA_ADDR=$3
+	A=$(echo $4 | awk NF=NF FS=)
 	for i in $A; do
-		B=$(printf '%x' "'$i")
-		write_i2c_byte $DATA_ADDR $B
+		B=$(printf '0x%x' "'$i")
+		write_i2c_byte $1 $2 $DATA_ADDR $B
 		let DATA_ADDR="$DATA_ADDR + 1"
 	done
 }
@@ -65,15 +74,17 @@ write_i2c_string()
 # zero at the end of the string
 #
 # params:
-# 1: data addr
-# 2: value
+# 1: i2c bus
+# 2: chip addr
+# 3: data addr
+# 4: value
 write_i2c_mac()
 {
-	DATA_ADDR=$1
-	A=$(echo $2 | grep -o ..)
+	DATA_ADDR=$3
+	A=$(echo $4 | grep -o ..)
 	for i in $A; do
-		B=$(printf %s "$i")
-		write_i2c_byte $DATA_ADDR $B
+		B=$(printf 0x%s "$i")
+		write_i2c_byte $1 $2 $DATA_ADDR $B
 		let DATA_ADDR="$DATA_ADDR + 1"
 	done
 }
@@ -122,18 +133,6 @@ mac_is_valid()
 	esac
 }
 
-dram_size_is_valid()
-{
-	case $1 in
-		[1-4])
-			return 0
-			;;
-		*)
-			return 1;
-			;;
-	esac
-}
-
 fail()
 {
 	echo -e "FAIL: $@"
@@ -145,7 +144,6 @@ fail()
 ######################################################################
 
 SOC="MX8MM"
-SOM_REV="01"
 
 echo -n "Enter Part Number: VSM-DT8MM-"
 read -e PN
@@ -202,11 +200,11 @@ if ! mac_is_valid $MAC; then
 	fail "Invalid MAC"
 fi
 
-
 # Program EEPROM fields
-write_i2c_string  ${PN_OFFSET}	  ${PN}
-write_i2c_string  ${AS_OFFSET}	  ${AS}
-write_i2c_string  ${DATE_OFFSET}  ${DATE}
-write_i2c_mac     ${MAC_OFFSET}	  ${MAC}
-write_i2c_byte    ${SR_OFFSET}	  ${SOM_REV}
-write_i2c_byte    ${OPT_OFFSET}	  ${SOM_OPTIONS}
+write_i2c_string  ${I2C_BUS} ${I2C_ADDR} ${PN_OFFSET}	  ${PN}
+write_i2c_string  ${I2C_BUS} ${I2C_ADDR} ${AS_OFFSET}	  ${AS}
+write_i2c_string  ${I2C_BUS} ${I2C_ADDR} ${DATE_OFFSET}   ${DATE}
+write_i2c_mac     ${I2C_BUS} ${I2C_ADDR} ${MAC_OFFSET}	  ${MAC}
+write_i2c_byte    ${I2C_BUS} ${I2C_ADDR} ${SR_OFFSET}	  ${SOM_REV}
+write_i2c_byte    ${I2C_BUS} ${I2C_ADDR} ${OPT_OFFSET}	  ${SOM_OPTIONS}
+
