3b394aca0b9efbafd723ab2acd3f44985f33d4d0
Felix Radensky <felix.r@variscite.com>
2019-06-02 15:11:44 +0300

    mx8mm_vareeprom_dram.sh: Add EPPROM signature verification

--- mx8mm_vareeprom_dram.sh
+++ mx8mm_vareeprom_dram.sh
@@ -5,10 +5,12 @@ EEPROM_IMAGE=/run/media/sda1/ddr/DART-MX8M-MINI-Samsung-2G-K4F6E3S4HM-MGCJ.bin
 EEPROM_SYSFS=/sys/devices/platform/30a20000.i2c/i2c-0/0-0052/eeprom
 IMAGE_SIZE=$(ls -l ${EEPROM_IMAGE} | awk '{print $5}')
 EEPROM_TEMP=/tmp/eeprom.$$
+MAGIC=/tmp/magic.$$
+MAGIC_TEMP=/tmp/magic_temp.$$
 
 cleanup()
 {
-	rm -f ${EEPROM_TEMP}
+	rm -f ${EEPROM_TEMP} ${MAGIC} ${MAGIC_TEMP}
 }
 
 trap cleanup EXIT
@@ -34,8 +36,18 @@ if ! cmp ${EEPROM_IMAGE} ${EEPROM_TEMP}; then
   exit 1
 fi
 
-# Write EEPROM signature
-echo -n -e '\x38\x4d' | dd of=${EEPROM_SYSFS} bs=1
+# Write EEPROM signature and verify that write was successful
+echo -n -e '\x38\x4d' > ${MAGIC}
+dd if=${MAGIC} of=${EEPROM_SYSFS} bs=1
+sleep 1
+
+echo 3 > /proc/sys/vm/drop_caches
+dd if=${EEPROM_SYSFS} of=${MAGIC_TEMP} bs=1 count=2
+if ! cmp ${MAGIC} ${MAGIC_TEMP}; then
+  echo "EEPROM magic write failed"
+  exit 1
+fi
+
 echo "EEPROM write successful"
 
 exit 0
